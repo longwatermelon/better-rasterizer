b2721fdeea24be85fbfa84fa2652ea92ac7f2346
longwatermelon <longwatermelon1@gmail.com>
2021-07-26 13:42:41 -0700

    remove old commented out code

--- src/graphics.cpp
+++ src/graphics.cpp
@@ -62,54 +62,5 @@ void graphics::draw_filled_triangle(SDL_Renderer* rend, SDL_FPoint p1, SDL_FPoin
         int max = std::max(xl[y], xr[y]);
         SDL_RenderDrawLine(rend, min, y, max, y);
     }
-
-#if 0
-    auto interpolate = [](float y1, float x1, float y2, float x2) {
-        if (y1 == y2)
-            return std::vector<float>{ x2 };
-
-        std::vector<float> values;
-        float slope = (x2 - x1) / (y2 - y1);
-        float x = x1;
-
-        for (int i = y1; i < y2; ++i)
-        {
-            values.emplace_back(x);
-            x += slope;
-        }
-
-        return values;
-    };
-
-    std::vector<float> x01 = interpolate(p1.y, p1.x, p2.y, p2.x);
-    std::vector<float> x12 = interpolate(p2.y, p2.x, p3.y, p3.x);
-    std::vector<float> x02 = interpolate(p1.y, p1.x, p3.y, p3.x);
-
-    if (x01.empty() || x12.empty() || x02.empty())
-        return;
-
-    x01.insert(x01.end(), x12.begin(), x12.end());
-
-    int middle = (int)(x01.size() / 2);
-
-    std::vector<float>* x_left;
-    std::vector<float>* x_right;
-
-    if (x02[middle] < x01[middle])
-    {
-        x_left = &x02;
-        x_right = &x01;
-    }
-    else
-    {
-        x_left = &x01;
-        x_right = &x02;
-    }
-
-    for (int y = p1.y; y <= p3.y; ++y)
-    {
-        SDL_RenderDrawLine(rend, (*x_left)[(int)(y - p1.y)], y, (*x_right)[(int)(y - p1.y)], y);
-    }
-#endif
 }
 
